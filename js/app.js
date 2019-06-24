712bce8e5b135b6e38b50950daa51e2db71f9d8f
eminzzz <eminroth@gmail.com>
2019-06-25 00:20:36 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let numberType = 123,
+    stringType = 'String Type',
+    booleanType = false,
+    nullType = null,
+    undefinedType = undefined,
+    objectType = {
+        'key': 'objectType'
+    };
+console.log(numberType, stringType, booleanType, nullType, undefinedType, objectType)
